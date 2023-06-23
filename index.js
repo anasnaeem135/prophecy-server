6bb6adb8c8f002b79150a3f583e381e7cb34f2c3
Anas Naeem <95508485+anasnaeem135@users.noreply.github.com>
2023-06-23 15:44:14 +0500

    Advertisment Api

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+const cors = require("cors");
+const axios = require("axios");
+const multer = require("multer");
 const express = require("express");
 const mongoose = require("mongoose");
-const UserSchema = require("./models/channel");
-const server = express();
-const cors = require("cors");
 const bodyParser = require("body-parser");
-const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const server = express();
+
 server.use(cors());
 server.use(bodyParser.json());
+
+const UserSchema = require("./models/channel");
 
 const DB =
   "mongodb+srv://anas:forbidden@prophecy.lmsu61v.mongodb.net/Prophecy?retryWrites=true&w=majority";
@@ -24,6 +30,7 @@ server.listen(8080, () => {
   console.log("Server has started");
 });
 
+// signup Api
 server.post("/signup", async (req, res) => {
   const { body } = req;
 
@@ -52,7 +59,6 @@ server.post("/signup", async (req, res) => {
 });
 
 // login API
-
 server.post("/login", async (req, res) => {
   const { body } = req;
 
@@ -65,12 +71,66 @@ server.post("/login", async (req, res) => {
 
   if (user) {
     //user exists
-    res.status(200).send({ message: "Logged in successfully!" });
+    user.password = null;
+    res.status(200).send({ message: "Logged in successfully!", user });
     return true;
   }
   res.status(404).send({ message: "Invalid Email or Password" });
   return false;
   //user doesn't exist
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "advertisments/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage }).single("file");
+
+server.post("/uploadAdvertisment", function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.status(500).send({ message: "Advertisment upload failed" });
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.status(500).send({ message: "Advertisment upload failed" });
+    }
+    res.status(200).send({ message: "Advertisment uploaded successfully!" });
+    // Everything went fine.
+  });
+});
+
+server.get("/getAdvertisments", (req, res) => {
+  const imagesFolder = path.join(__dirname, "images");
+
+  fs.readdir(imagesFolder, (err, files) => {
+    if (err) {
+      console.error("Error reading images folder:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    // Filter out non-image files if necessary
+    const imageFiles = files.filter((file) => {
+      const extension = path.extname(file).toLowerCase();
+      return [".jpg", ".jpeg", ".png"].includes(extension);
+    });
+
+    // Create an array of image URLs or image data
+    const images = imageFiles.map((file) => {
+      const imageUrl = `/images/${file}`;
+      // If you want to send base64-encoded image data instead of URLs:
+      // const imageData = fs.readFileSync(path.join(imagesFolder, file), 'base64');
+      return imageUrl;
+    });
+
+    res.json(images);
+  });
 });
 
 //
@@ -85,7 +145,7 @@ server.get("/crypto", async (req, res) => {
             "X-CMC_PRO_API_KEY": "8c778a4e-f1ae-42b9-abe0-efb9a870d00e",
           },
           params: {
-            symbol: "BTC,SOL,DOGE,DOT",
+            symbol: "BTC,SOL,DOGE,DOT,BNB,SHIB",
           },
         }
       );
@@ -134,3 +194,59 @@ server.get("/cricket", async (req, res) => {
     }
   });
 });
+
+// server.get("/football", async (req, res) => {
+//   let response = null;
+//   new Promise(async (resolve, reject) => {
+//     try {
+//       const id = 2224;
+//       response = await axios.get(
+//         "https://api-football-v1.p.rapidapi.com/v3/fixtures",
+//         {
+//           params: { next: "50" },
+//           headers: {
+//             "X-RapidAPI-Key":
+//               "577412bbd5mshc7dc2a58368aa4ap180df4jsna48c9bc6659a",
+//             "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+//           },
+//         }
+//       );
+//     } catch (ex) {
+//       response = null;
+//       // error
+//       console.log(ex);
+//       reject(ex);
+//     }
+//     if (response) {
+//       // success
+//       const json = response.data;
+
+//       res.json(json);
+//       resolve(json);
+//     }
+//   });
+// });
+
+// server.get("/football", async (req, res) => {
+// const axios = require("axios");
+
+// const options = {
+//   method: "GET",
+//   url: "https://livescore6.p.rapidapi.com/matches/v2/list-live",
+//   params: {
+//     Category: "soccer",
+//     Timezone: "-7",
+//   },
+//   headers: {
+//     "X-RapidAPI-Key": "577412bbd5mshc7dc2a58368aa4ap180df4jsna48c9bc6659a",
+//     "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
+//   },
+// };
+// new Promise(async (resolve, reject) => {
+//   try {
+//     const response = await axios.request(options);
+//     console.log("Soccer data : ", response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
